b36140d7ed6051550803c428d267896610c288c6
Jonas Schneider <mail@jonasschneider.com>
2014-01-02 22:41:26 +0100

    protected mode, yay!

--- boot.s
+++ boot.s
@@ -3,26 +3,85 @@
 
 # Export the _start symbol, which is by convention the entry point for the .text
 # section. Our makefile places the beginning of the text section at the start of
-# the MBR.
+# the MBR. At runtime, the BIOS loads the MBR into address 0x7c00.
 .globl  _start
 _start:
-  # Immediately jump to after the string that follows (it'd be an illegal instruction).
-  # The string is placed near the beginning so that it's at a known location
-  # (The BIOS loads the MBR into address 0x7c00, and the jump is 16 bits, so the string
-  #  is known to start at 0x7c02. Ultra hacky, but ok.)
-  jmp go
-
-.string "Hello from the grid.\r\nEntering protected mode...\0"
-
-go:
-  mov $0x7c02, %bx
-  call print_str_BX
-
   # Let's enter protected mode.
-  # http://wiki.osdev.org/Protected_Mode
-  #cli
-  #lgdt %gdtr
 
+  # Disable interrupts - apparently that's a thing.
+  cli
+
+  # So protected mode has this thing called segmentation. A table of segments
+  # contains a list of segment descriptors. These are blobs of virtual memory
+  # of arbitrary size that are mapped to physical memory by the MMU.
+  # Additionally, segmentation can provide write and execute protection for
+  # some segments.
+  # It looks like Real OS's (tm) only touch segmentation as little as possible,
+  # and generally try to use paging for protection.
+
+  # Load our Global Descriptor Table (GDT). This table contains our segment descriptors.
+  # We only use segementation very lightly. The addressing calculation is a bit of linker
+  # cheating - we're basically telling the assemlber that we know that _start is going
+  # to be at 0x7c00, and that we want all addresses calculated relative to that known
+  # point of information.
+  lgdt (gdt_descriptor - _start + 0x7c00)
+
+  # To formally enter protected mode, we set the protected bit on the CR0.
+  # http://en.wikipedia.org/wiki/Control_register
+  mov %cr0, %eax
+  or $1, %eax # set Protected bit #0
+  mov %eax, %cr0
+
+  # Now we are half-way in protected mode - our current segment (CS register) is still 0
+  # though, which is murkily defined to be somewhat illegal.
+  # Before we fix that, though, we have to clear the CPU's instruction pipeline, since
+  # otherwise half-decoded instructions after here will still be executed in Real mode
+  # (a microcode thing, apparently), which isn't really all that cool.
+  # What better way to do that than a no-op jump!
+  jmp after_clear_pipeline
+  after_clear_pipeline:
+
+  # Now we finally get ready to make the jump to our first real segment.
+  # To do this, we do a long jump. Long jumping means that we're breaking the abstraction of
+  # virtual memory by switching segments.
+  # The arguments to ljmp are the segment selector, and the offset.
+  # Concatenated, they probably form a virtual memory address.
+  # Since all our segments map to the entire physical memory, we want to
+  # The segment selector is 13 bits of an index into the GDT, 1 bit that is set
+  # if we're looking at the LDT, and 2 bits that specify the protection level to access.
+  # Since we don't have any Local Descriptor Tables (LDTs) set, we want to look at the GDT,
+  # and we want to stay in Protection Level 0, the innermost ring.
+  # Our index into the GDT is 1 (the kernel code segment that comes past the null segment).
+  # This will also cause the CPU to switch out of 16-bit mode; the instruction that we'll
+  # arrive at after the jump will be decoded as 32-bit.
+  # We'd have to do the inverse of the MMU's work here, but since within a segment,
+  # physical and logical addresses are identical, we can just pass the location of our jump target.
+  # So long, 8086 mode!
+  ljmp $0b1000, $(in_prot32 - _start + 0x7c00)
+
+# This is some strange metadata struct that points to the GDT.
+gdt_descriptor:
+  .word (3 * 8) - 1 # GDT size in bytes - 1, 3 is the number of entries
+  .word gdt - _start + 0x7c00
+
+gdt:
+  # Null Descriptor
+  .word 0x0000, 0x0000
+  .byte 0x00, 0b00000000, 0b00000000, 0x00
+  # Code Descriptor
+  .word 0xffff, 0x0000
+  .byte 0x00, 0b10011010, 0b11001111, 0x00
+  # Data Descriptor
+  .word 0xffff, 0x0000
+  .byte 0x00, 0b10010010, 0b11001111, 0x00
+
+# This is the first procedure called after completing the switch to protected mode.
+# (Might want to re-enable interrupts again?)
+in_prot32:
+.code32
+  # Just print a fancy message here
+  mov $(startup_message - _start + 0x7c00), %eax
+  call print_str_eax
 
   # It's unclear what the processor will do when we just stop doing anything here.
   # It'll probably start executing null byte instructions or something else silly.
@@ -30,26 +89,28 @@ go:
 hang:
   jmp hang
 
+startup_message:
+.string "Hello from the grid, speaking to you from protected mode.\0"
 
-
-print_str_BX:
-  # BX (Base Index) is one of the few 16-bit registers that can be
-  # used for address arithmetic. I'm not yet sure why, but it apparently has something to
-  # do with segments (x86 in Real Mode has a 20-bit address space, but only 16-bit registers).
-  # Cf. http://f.osdev.org/viewtopic.php?f=13&t=18374
-
-  # Load the next byte to print into AL, exit if it's \0.
-  mov (%bx), %al
-  test %al, %al
+# Print the null-terminated string starting at %eax on the first line of the screen.
+print_str_eax:
+  # Apparently, once we're out of real mode, we can use ALL the registers however we want.
+  mov $0, %ebx
+char:
+  movb (%eax, %ebx), %ecx
+  test %ecx, %ecx
   jz print_done
 
-  # Invoke INT10/AH=0E, which is by convention a BIOS call to print the character in AL.
-  mov $0x0e, %ah
-  int $0x10
+  # Thank god we don't have to actually mess with pixels.
+  # The system provides a video buffer that starts at 0xB8000. For each character (40x25?),
+  # 2 bytes are stored; the first byte is the ASCII code, and the second byte is formatting.
+  # I read somewhere that the formatting is 4 bits each for foreground and background color,
+  # and a value of 7 is grey on black.
+  movb %ecx, 0xB8000(,%ebx,2)
+  movb $7, 0xB8001(,%ebx,2)
 
-  # Advance the counter, and rinse
-  inc %bx
-  jmp print_str_BX
+  inc %ebx
+  jmp char
 
 print_done:
   ret
