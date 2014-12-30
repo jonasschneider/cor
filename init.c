d96b9eec18047f46379c8baca52d37159061768c
Jonas Schneider <mail@jonasschneider.com>
2014-12-30 10:35:10 +0100

    hard-code everything to get message from init working

--- init.c
+++ init.c
@@ -1,5 +1,11 @@
+
+
 void _start() {
-  char *str = "Hello, world from kernel-space loaded init (i'm not even kidding)\n";
+  char *str = "Hello, world from kernel-space loaded init (i'm not even kidding..)\n";
+  int (*cor_printk)(const char *format, ...);
+  cor_printk = (int (* )(const char *format, ...))0x000000000001048a;
+  cor_printk(str-0x400000+0x70000);
+
   __asm__ ( "hlt" : : "a" (str) );
   while(1);
 }
