0edec6fa8aa172e7858085302d998c798d1c656a
Jonas Schneider <mail@jonasschneider.com>
2014-12-30 03:39:57 +0100

    optimize for the happy path

--- test/integration_test.go
+++ test/integration_test.go
@@ -4,8 +4,9 @@ import (
   "testing"
   "os/exec"
   "os"
-  "bytes"
+  "bufio"
   "time"
+  "io"
 )
 
 var cmds = make([]*exec.Cmd, 0)
@@ -13,23 +14,39 @@ var cmds = make([]*exec.Cmd, 0)
 func TestStartupSerialMessage(t *testing.T) {
   cmd := exec.Command(os.Getenv("ROOT")+"/bin/run")
   cmd.Dir = os.Getenv("ROOT")
-  var buf bytes.Buffer
-  cmd.Stdout = &buf
+
+  rd, wr := io.Pipe()
+  lines := bufio.NewReader(rd)
+  cmd.Stdout = wr
+
   err := cmd.Start()
   if err != nil {
     t.Error(err)
   }
+
+  rcv := make(chan string)
+  go func() {
+    s, err := lines.ReadBytes('\n')
+    if err != nil {
+      t.Error(err)
+    }
+    rcv <- string(s)
+  }()
+
   cmds = append(cmds, cmd)
-  <-time.After(500*time.Millisecond)
-  msg := string(buf.Bytes())
-  needle := "Switched to serial console."
-  if len(msg) < len(needle) {
-    t.Error("needle is",len(needle),"long, but msg has only",len(msg))
-    return
-  }
-  match := msg[0:len(needle)]
-  if needle != match {
-    t.Error("expected match:",needle,match)
+  select {
+  case msg := <- rcv:
+    needle := "Switched to serial console."
+    if len(msg) < len(needle) {
+      t.Error("needle is",len(needle),"long, but msg has only",len(msg))
+      return
+    }
+    match := msg[0:len(needle)]
+    if needle != match {
+      t.Error("expected match:",needle,match)
+    }
+  case <-time.After(1000*time.Millisecond):
+    t.Error("timed out without reading anything")
   }
 }
 
