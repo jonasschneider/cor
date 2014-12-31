2b899cf7b9a28d4263358a83266cad9edbb266f8
Jonas Schneider <mail@jonasschneider.com>
2014-12-31 13:08:02 +0100

    fixup cor_printk address

--- init.c
+++ init.c
@@ -1,7 +1,7 @@
 void _start() {
   char *str = "Hello, world from kernel-space init\nI am loaded at %x (not even kidding..)\n";
   int (*cor_printk)(const char *format, ...);
-  cor_printk = (int (* )(const char *format, ...))0x000000000001048a;
+  cor_printk = (int (* )(const char *format, ...))0x0000000000010641;
 
   cor_printk(str, _start);
 
