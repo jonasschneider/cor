96149aabc8470c33ff5f1e9d7d08f9b4f2ffe210
Jonas Schneider <mail@jonasschneider.com>
2015-12-29 19:56:05 +0100

    loop for shell input

--- userspace/init.c
+++ userspace/init.c
@@ -6,12 +6,15 @@ int write(int fd, const void *buf, size_t count);
 
 int main() {
   int fd = open("/dev/console", 0);
-  write(fd, "> ", 2);
 
-  char buf[256] = {0};
-  read(fd, buf, 256);
+  while(1) {
+    write(fd, "> ", 2);
 
-  printf("You said: '%s'", buf);
+    char buf[256] = {0};
+    read(fd, buf, 256);
+
+    printf("%s\n", buf);
+  }
 
   return 0;
 }
