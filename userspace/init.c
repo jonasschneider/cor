5b995fe8cbfb17ac0d98db94521b1c4fe4fbd39b
Jonas Schneider <mail@jonasschneider.com>
2015-12-29 20:16:41 +0100

    echo shell builtin

--- userspace/init.c
+++ userspace/init.c
@@ -8,12 +8,24 @@ int main() {
   int fd = open("/dev/console", 0);
 
   while(1) {
-    write(fd, "> ", 2);
+    write(fd, "$ ", 2);
 
     char buf[256] = {0};
     read(fd, buf, 256);
 
-    printf("%s\n", buf);
+    int pos = 0;
+    while(buf[pos] != ' ' && buf[pos] != '\n') {
+      pos++;
+    }
+    buf[pos] = 0; // set space to \0
+
+    if(pos == 0) {
+      printf("");
+    } else if(buf[0] == 'e' && buf[1] == 'c' && buf[2] == 'h' && buf[3] == 'o' && buf[4] == 0) {
+      printf("%s", buf+pos+1);
+    } else {
+      printf("sh: %s: command not found\n", buf);
+    }
   }
 
   return 0;
