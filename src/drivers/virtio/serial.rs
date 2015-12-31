e103566ac130ad388be7dff439b6d55bb9f6ea48
Jonas Schneider <mail@jonasschneider.com>
2015-12-31 11:31:45 +0100

    cleanup

--- src/drivers/virtio/serial.rs
+++ src/drivers/virtio/serial.rs
@@ -1,31 +1,22 @@
 use prelude::*;
 
-use core::borrow::BorrowMut;
-
 use cpuio;
-use super::{vring,InitError};
+use super::{virtq,InitError};
+
 const VIRTIO_STATUS_ACKNOWLEDGE: u8 = 1;
 const VIRTIO_STATUS_DRIVER: u8 = 2;
 const VIRTIO_STATUS_DRIVER_OK: u8 = 4;
 const VIRTIO_STATUS_FAILED: u8 = 128;
 
-const VRING_DESC_F_NEXT: u16 = 1; /* This marks a buffer as continuing via the next field. */
-const VRING_DESC_F_WRITE: u16 = 2; /* This marks a buffer as write-only (otherwise read-only). */
-
-use super::vring::Descriptor;
 use mem::*;
 
 use sched;
 
-extern {
-  fn asm_eoi();
-}
-
 pub struct Serialdev {
   port: cpuio::IoPort,
 
-  rxq: super::virtq::Virtq,
-  txq: super::virtq::Virtq,
+  rxq: virtq::Virtq,
+  txq: virtq::Virtq,
 }
 
 impl Serialdev {
@@ -47,11 +38,10 @@ impl Serialdev {
         lock.pop_front()
       };
 
-      if let Some((rxbuf, read)) = r {
-        n = read;
+      if let Some((rxbuf, count)) = r {
+        n = count;
 
         buf.clone_from_slice(&rxbuf.1[0..n]);
-        println!("read: {:?}", &rxbuf);
 
         // enqueue the buffer again for the next read
         self.rxq.free_buffers.lock().push_back(rxbuf);
@@ -59,6 +49,7 @@ impl Serialdev {
 
         break;
       }
+
       sched::kyield();
     }
 
@@ -72,7 +63,7 @@ impl Serialdev {
       "XXXXXXXX----------X-XXXX",  // feature negotiation flags, device status, MSI-X fields
       "--------XXXXXXXXXX-X----"); // queue address, size, select, notify, ISR status
 
-    let (mut rxport, mut txport) = operationsport.split_at_masks(
+    let (mut irqport, mut userport) = operationsport.split_at_masks(
       "-------------------X----",  // ISR status
       "--------XXXXXXXXXX------"); // queue address, size, select, notify
 
@@ -90,58 +81,46 @@ impl Serialdev {
     println!("The device offered us these feature bits: {:?}", offered_featureflags);
     // In theory, we'd do `negotiated = offered & supported`; we don't actually
     // support any flags, so we can just set 0.
-    configport.write16(4, 0); // XX
-
-    // Discover virtqueues; the block devices only has one
+    configport.write16(4, 0);
+    // TODO: handle featureflags; a PORT_OPEN(0) ctrl message (indicating the port was closed)
+    // is sent after EOF on qemu's stdin if the multiport feature was negotiated
     if configport.read16(4) != 0 {
       return Err(InitError::VirtioHandshakeFailure)
     }
 
-    // RX
+    // Set up the rx & tx virtqueues.
 
-    // Behaviour: Move stuff into an "unread queue" -> TODO: encapsulation
-    let (mut rxq, rxrecv) = super::virtq::Virtq::new(0, &mut txport, box move |used, free| {
+    // Rx Behaviour: Move stuff into an "unread queue" -> TODO: encapsulation
+    let (mut rxq, rxrecv) = virtq::Virtq::new(0, &mut userport, box move |used, free| {
       println!("serialrx processing used buffers: {:?}", used);
     });
     for _ in 0..1 {
       rxq.register(box ['X' as u8; 20], true); // writable by them
-      rxq.send(&[0u8; 20], &mut txport);
+      rxq.send(&[0u8; 20], &mut userport);
     }
 
-    // TX
-
-    // Behaviour: When the device consumes a tx buffer, we simply re-queue
+    // Tx Behaviour: When the device consumes a tx buffer, we simply re-queue
     // it as a free buffer.
-    let (mut txq, txrecv) = super::virtq::Virtq::new(1, &mut txport, box move |used, free| {
+    let (mut txq, txrecv) = virtq::Virtq::new(1, &mut userport, box move |used, free| {
       use core::iter::Extend;
 
       println!("serialtx processing used buffers: {:?}", used);
       free.lock().extend(used.drain(..).map(|(buf, read)| buf));
     });
     for _ in 0..10 {
-      txq.register(box ['X' as u8; 1], false);
+      txq.register(box ['X' as u8; 1], false); // not writable by them
     }
 
-    let handler = super::virtq::RxHandler {
+    let handler = virtq::RxHandler {
       rings: vec![rxrecv, txrecv],
-      isr_status_port: rxport,
+      isr_status_port: irqport,
     };
 
     sched::irq::add_handler(0x2b, box handler);
 
-
-    // Tell the device we're done setting it up
     state = state | VIRTIO_STATUS_DRIVER_OK;
     configport.write8(18, state);
-    println!("Device state is now: {}", state);
 
-    // TODO: a PORT_OPEN(0) ctrl message (indicating the port was closed) is sent after EOF on qemu's stdin
-
-    let mut dev = Serialdev { port: txport, rxq: rxq, txq: txq};
-
-    dev.putc('?');
-    dev.putc('!');
-
-    Ok(dev)
+    Ok(Serialdev { port: userport, rxq: rxq, txq: txq })
   }
 }
